85094fc4393bf5386923a870239b10a1ba666348
Niharika <nihasrinu.k@gmail.com>
2025-08-10 19:19:37 +0530

    updated

--- src/main/java/com/klef/ClientController.java
+++ src/main/java/com/klef/ClientController.java
@@ -18,7 +18,7 @@ public class ClientController
 	 @GetMapping("/demo")
 	   public String demo()
 	   {
-		   return "Jenkins erp";
+		   return "Jenkins erp1";
 	   }
 	   
    @GetMapping("/test")
